ab8cb5d9cb1663f708d634fe0132a7de60b2df27
James Deathe <james.deathe@jadu.co.uk>
2016-01-06 16:22:46 +0000

    ISSUE 61: Refactor bootstrap configuration validation functions.

--- run.sh
+++ run.sh
@@ -101,8 +101,8 @@ docker run \
 # 	-d \
 # 	--name ${DOCKER_NAME} \
 # 	-p :22 \
-# 	--env "SSH_USER=app-admin" \
-# 	--env "SSH_USER_HOME_DIR=/home/app-admin" \
+# 	--env "SSH_USER=app-1" \
+# 	--env "SSH_USER_HOME_DIR=/home/app-1" \
 # 	--env "SSH_USER_SHELL=/bin/sh" \
 # 	--env "SSH_AUTHORIZED_KEYS=
 # ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key
