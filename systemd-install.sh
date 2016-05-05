049ea0839d72f83fea2351a1d3cc6186b5aecfe0
James Deathe <james.deathe@jadu.co.uk>
2016-05-05 18:50:27 +0100

    ISSUE 190: Update systemd installer script to replace etcd service name on non CoreOS distributions. Added etcd service registration / de-registration steps back in.

--- systemd-install.sh
+++ systemd-install.sh
@@ -13,11 +13,46 @@ fi
 
 source run.conf
 
+is_coreos_distribution ()
+{
+	if [[ -n $( [[ -e /etc/os-release ]] && grep ^ID=coreos$ /etc/os-release ) ]]; then
+		return 0
+	fi
+
+	return 1
+}
+
+replace_etcd_service_name ()
+{
+	local FILE_PATH=${1}
+
+	if [[ -z ${FILE_PATH} ]]; then
+		echo "Path to the service's unit file is required."
+		return 1
+	fi
+
+	if ! [[ -s ${FILE_PATH} ]]; then
+		echo "Unit file not found."
+		return 1
+	fi
+
+	# CoreOS uses etcd.service and etcd2.service for version 1 and 2 of ETCD 
+	# respectively but has both available. Use etcd2.service in the systemd 
+	# unit file and rename for other distributions where etcd.service is the 
+	# only name used.
+	if ! is_coreos_distribution; then
+		echo "---> Not a CoreOS distribution."
+		echo " ---> Renaming etcd2.service to etcd.service in unit file."
+		sed -i -e 's~etcd2.service~etcd.service~g' ${FILE_PATH}
+	fi
+}
+
 SERVICE_UNIT_LONG_NAME=${SERVICE_UNIT_LONG_NAME:-ssh.pool-1.1.1}
 SERVICE_UNIT_FILE_NAME=${SERVICE_UNIT_FILE_NAME:-${SERVICE_UNIT_LONG_NAME}@2020.service}
 
 # Copy systemd definition into place and enable it.
 cp ${SERVICE_UNIT_FILE_NAME} /etc/systemd/system/
+replace_etcd_service_name /etc/systemd/system/${SERVICE_UNIT_FILE_NAME}
 systemctl daemon-reload
 systemctl enable -f /etc/systemd/system/${SERVICE_UNIT_FILE_NAME}
 
