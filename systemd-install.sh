20e12384083587ab7290188a57731e5f6c74a668
James Deathe <james.deathe@gmail.com>
2016-07-27 15:37:23 +0100

    ISSUE 311: Refactor Systemd install method to use an install template.

--- systemd-install.sh
+++ systemd-install.sh
@@ -9,24 +9,36 @@ source install.conf
 
 # Abort if systemd not supported
 if ! type -p systemctl &> /dev/null; then
-	printf -- "${COLOUR_NEGATIVE}--->${COLOUR_RESET} %s\n" 'Systemd installation not supported.'
+	printf -- \
+		"${COLOUR_NEGATIVE}--->${COLOUR_RESET} %s\n" \
+		'Systemd installation not supported.'
 	exit 1
 fi
 
 # Abort if not run by root user or with sudo
 if [[ ${EUID} -ne 0 ]]; then
-	printf -- "${COLOUR_NEGATIVE}--->${COLOUR_RESET} %s\n" 'Please run as root.'
+	printf -- \
+		"${COLOUR_NEGATIVE}--->${COLOUR_RESET} %s\n" \
+		'Please run as root.'
 	exit 1
 fi
 
-printf -- "---> Installing %s\n" ${SERVICE_UNIT_INSTANCE_NAME}
+printf -- \
+	"---> Installing %s\n" \
+	${SERVICE_UNIT_INSTANCE_NAME}
 
 # Copy systemd unit-files into place.
-cp ${SERVICE_UNIT_TEMPLATE_NAME} /etc/systemd/system/
-cp ${SERVICE_UNIT_REGISTER_TEMPLATE_NAME} /etc/systemd/system/
+cat \
+	${SERVICE_UNIT_INSTALL_TEMPLATE_NAME} \
+	> /etc/systemd/system/${SERVICE_UNIT_TEMPLATE_NAME}
+
+sed \
+	-e "s~{{SERVICE_UNIT_NAME}}~${SERVICE_UNIT_NAME}~g" \
+	-e "s~{{SERVICE_UNIT_GROUP}}~${SERVICE_UNIT_GROUP}~g" \
+	${SERVICE_UNIT_REGISTER_INSTALL_TEMPLATE_NAME} \
+	> /etc/systemd/system/${SERVICE_UNIT_REGISTER_TEMPLATE_NAME}
 
 systemctl daemon-reload
-
 systemctl enable -f ${SERVICE_UNIT_INSTANCE_NAME}
 systemctl enable -f ${SERVICE_UNIT_REGISTER_INSTANCE_NAME}
 systemctl restart ${SERVICE_UNIT_INSTANCE_NAME} &
@@ -40,16 +52,32 @@ PIDS[1]=${!}
 [[ -n ${PIDS[0]} ]] && wait ${PIDS[0]}
 
 # Allow time for the container bootstrap to complete
-sleep 5
+sleep ${SERVICE_UNIT_INSTALL_TIMEOUT}
 kill -15 ${PIDS[1]}
 wait ${PIDS[1]} 2> /dev/null
 
-if systemctl -q is-active ${SERVICE_UNIT_INSTANCE_NAME} && systemctl -q is-active ${SERVICE_UNIT_REGISTER_INSTANCE_NAME}; then
-	printf -- "---> Service unit is active: %s\n" "$(systemctl list-units --type=service | grep "^[ ]*${SERVICE_UNIT_INSTANCE_NAME}")"
-	printf -- "---> Service register unit is active: %s\n" "$(systemctl list-units --type=service | grep "^[ ]*${SERVICE_UNIT_REGISTER_INSTANCE_NAME}")"
-	printf -- "${COLOUR_POSITIVE} --->${COLOUR_RESET} %s\n" 'Install complete'
+if systemctl -q is-active ${SERVICE_UNIT_INSTANCE_NAME} \
+	&& systemctl -q is-active ${SERVICE_UNIT_REGISTER_INSTANCE_NAME}; then
+	printf -- \
+		"---> Service unit is active: %s\n" \
+		"$(
+			systemctl list-units --type=service \
+			| grep "^[ ]*${SERVICE_UNIT_INSTANCE_NAME}"
+		)"
+	printf -- \
+		"---> Service register unit is active: %s\n" \
+		"$(
+			systemctl list-units --type=service \
+			| grep "^[ ]*${SERVICE_UNIT_REGISTER_INSTANCE_NAME}"
+		)"
+	printf -- \
+		"${COLOUR_POSITIVE} --->${COLOUR_RESET} %s\n" \
+		'Install complete'
 else
-	printf -- "\nService status:\n"
+	printf -- \
+		"\nService status:\n"
 	systemctl status -ln 50 ${SERVICE_UNIT_INSTANCE_NAME}
-	printf -- "\n${COLOUR_NEGATIVE} --->${COLOUR_RESET} %s\n" 'Install error'
+	printf -- \
+		"\n${COLOUR_NEGATIVE} --->${COLOUR_RESET} %s\n" \
+		'Install error'
 fi
