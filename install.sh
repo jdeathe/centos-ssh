aca010c350725c068ef2cb9fc36628b3c2a069ab
James Deathe <james.deathe@gmail.com>
2016-07-30 12:28:14 +0100

    ISSUE 311: Added bash/docker install method as default and Make manger optional.

--- install.sh
+++ install.sh
@@ -6,13 +6,266 @@ cd -- "$(
 )"
 
 source install.conf
+source environment.sh
+source default.sh
+
+# UI constants
+COLOUR_NEGATIVE='\033[1;31m'
+COLOUR_POSITIVE='\033[1;32m'
+COLOUR_RESET='\033[0m'
+CHARACTER_STEP='--->'
+PREFIX_STEP=$(
+	printf -- \
+		'%s ' \
+		"${CHARACTER_STEP}"
+)
+PREFIX_SUB_STEP=$(
+	printf -- \
+		' %s ' \
+		"${CHARACTER_STEP}"
+)
+PREFIX_STEP_NEGATIVE=$(
+	printf -- \
+		'%b%s%b' \
+		"${COLOUR_NEGATIVE}" \
+		"${PREFIX_STEP}" \
+		"${COLOUR_RESET}"
+)
+PREFIX_STEP_POSITIVE=$(
+	printf -- \
+		'%b%s%b' \
+		"${COLOUR_POSITIVE}" \
+		"${PREFIX_STEP}" \
+		"${COLOUR_RESET}"
+)
+PREFIX_SUB_STEP_NEGATIVE=$(
+	printf -- \
+		'%b%s%b' \
+		"${COLOUR_NEGATIVE}" \
+		"${PREFIX_SUB_STEP}" \
+		"${COLOUR_RESET}"
+)
+PREFIX_SUB_STEP_POSITIVE=$(
+	printf -- \
+		'%b%s%b' \
+		"${COLOUR_POSITIVE}" \
+		"${PREFIX_SUB_STEP}" \
+		"${COLOUR_RESET}"
+)
 
 INSTALL_SERVICE_REGISTER_ENABLED=false
+
+function docker_prerequisites ()
+{
+
+	docker=''
+
+	declare -a local DOCKER_PATHS=(
+		'/usr/bin/docker'
+		'/usr/local/bin/docker'
+	)
+
+	# Set the docker binary command
+	if [[ ${CHROOT_DIRECTORY} == / ]]; then
+		if ! command -v docker &> /dev/null; then
+			printf -- \
+				"${COLOUR_NEGATIVE}--->${COLOUR_RESET} %s\n" \
+				'ERROR: Missing docker binary'
+			exit 1
+		fi
+
+		printf -v \
+			docker \
+			-- '%s' \
+			$(command -v docker)
+	else
+		for DOCKER_PATH in "${DOCKER_PATHS[@]}"; do
+			if [[ -f ${CHROOT_DIRECTORY%*/}/${DOCKER_PATH} ]]; then
+				printf -v \
+					docker \
+					-- 'chroot %s %s' \
+					${CHROOT_DIRECTORY} \
+					${DOCKER_PATH}
+				break
+			fi
+		done
+
+		if [[ -z ${docker} ]]; then
+			printf -- \
+				"${COLOUR_NEGATIVE}--->${COLOUR_RESET} %s\n" \
+				'ERROR: Missing docker binary'
+			exit 1
+		fi
+	fi
+
+	# Test docker connection
+	if [[ -z $(${docker} info) ]]; then
+		printf -- \
+			"${COLOUR_NEGATIVE}--->${COLOUR_RESET} %s\n" \
+			'ERROR: Docker failed to connect to host.'
+		exit 1
+	fi
+
+	if [[ -z ${DOCKER_NAME} ]]; then
+		printf -- \
+			"${COLOUR_NEGATIVE}--->${COLOUR_RESET} %s\n" \
+			'ERROR: DOCKER_NAME not set.'
+		exit 1
+	fi
+}
+
+function docker_require_container ()
+{
+
+	if [[ -z $(${docker} ps -aq --filter "name=${DOCKER_NAME}") ]]; then \
+		echo "${PREFIX_STEP_NEGATIVE} This operation requires the ${DOCKER_NAME} docker container."; \
+		exit 1; \
+	fi
+
+}
+
+function docker_require_container_not ()
+{
+
+	if [[ -n $(${docker} ps -aq --filter "name=${DOCKER_NAME}") ]]; then \
+		echo "${PREFIX_STEP_NEGATIVE} This operation requires the ${DOCKER_NAME} docker container be removed (or renamed)."; \
+		echo "${PREFIX_SUB_STEP} Try removing it with: docker rm -f ${DOCKER_NAME}"; \
+		exit 1; \
+	fi
+
+}
+
+function docker_require_container_not_status_paused ()
+{
+
+	if [[ -n $(${docker} ps -aq --filter "name=${DOCKER_NAME}" --filter "status=paused") ]]; then \
+		echo "${PREFIX_STEP_NEGATIVE} This operation requires the ${DOCKER_NAME} docker container to be unpaused."; \
+		echo "${PREFIX_SUB_STEP} Try unpausing it with: docker ${DOCKER_NAME} unpause"; \
+		exit 1; \
+	fi
+
+}
+
+function docker_terminate ()
+{
+
+	if [[ -z $(${docker} ps -aq --filter "name=${DOCKER_NAME}") ]]; then \
+		echo "${PREFIX_STEP} Container termination skipped"; \
+	else \
+		echo "${PREFIX_STEP} Terminating container"; \
+		if [[ -n $(${docker} ps -aq --filter "name=${DOCKER_NAME}" --filter "status=paused") ]]; then \
+			echo "${PREFIX_SUB_STEP} Unpausing container"; \
+			${docker} unpause ${DOCKER_NAME} 1> /dev/null; \
+		fi; \
+		if [[ -n $(${docker} ps -aq --filter "name=${DOCKER_NAME}" --filter "status=running") ]]; then \
+			echo "${PREFIX_SUB_STEP} Stopping container"; \
+			${docker} stop ${DOCKER_NAME} 1> /dev/null; \
+		fi; \
+		if [[ -n $(${docker} ps -aq --filter "name=${DOCKER_NAME}") ]]; then \
+			echo "${PREFIX_SUB_STEP} Removing container"; \
+			${docker} rm -f ${DOCKER_NAME} 1> /dev/null; \
+		fi; \
+		if [[ -z $(${docker} ps -aq --filter "name=${DOCKER_NAME}") ]]; then \
+			echo "${PREFIX_SUB_STEP_POSITIVE} Container terminated"; \
+		else \
+			echo "${PREFIX_SUB_STEP_NEGATIVE} Container termination failed"; \
+			exit 1; \
+		fi; \
+	fi
+
+}
+
+function docker_create ()
+{
+
+	docker_require_container_not
+
+	echo "${PREFIX_STEP} Creating container"
+
+	(
+		set -x; \
+		eval "${docker} create \
+			${DOCKER_CONTAINER_PARAMETERS} \
+			${DOCKER_CONTAINER_PARAMETERS_APPEND} \
+			${DOCKER_USER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}" 1> /dev/null;
+	)
+
+	if [[ -n $(${docker} ps -aq --filter "name=${DOCKER_NAME}" --filter "status=created") ]]; then \
+		echo "${PREFIX_SUB_STEP} $(${docker} ps -aq --filter "name=${DOCKER_NAME}" --filter "status=created")"; \
+		echo "${PREFIX_SUB_STEP_POSITIVE} Container created"; \
+	else \
+		echo "${PREFIX_SUB_STEP_NEGATIVE} Container creation failed"; \
+		exit 1; \
+	fi
+
+}
+
+function docker_start ()
+{
+	docker_require_container
+	docker_require_container_not_status_paused
+
+	echo "${PREFIX_STEP} Starting container"
+
+	if [[ -n $(${docker} ps -aq --filter "name=${DOCKER_NAME}") ]] \
+		&& [[ -z $(${docker} ps -aq --filter "name=${DOCKER_NAME}" --filter "status=running") ]]; then \
+		${docker} start ${DOCKER_NAME} 1> /dev/null; \
+	fi
+
+	if [[ -n $(${docker} ps -aq --filter "name=${DOCKER_NAME}" --filter "status=running") ]]; then \
+		echo "${PREFIX_SUB_STEP_POSITIVE} Container started"; \
+	else \
+		echo "${PREFIX_SUB_STEP_NEGATIVE} Container start failed"; \
+		exit 1; \
+	fi
+}
+
+function docker_install ()
+{
+	docker_prerequisites
+	docker_terminate
+	docker_create
+	docker_start
+}
+
+function make_install ()
+{
+
+	local COMMAND
+	declare -a local LOCAL_COMMAND_PATHS=(
+		'/usr/bin/make'
+	)
+
+	for COMMAND_PATH in "${LOCAL_COMMAND_PATHS[@]}"; do
+		COMMAND=${COMMAND_PATH##*/}
+		if ! command -v ${COMMAND} &> /dev/null; then
+			printf -- \
+				"${COLOUR_NEGATIVE}--->${COLOUR_RESET} ERROR: Missing required command: %s\n" \
+				${COMMAND_PATH}
+			exit 1
+		fi
+
+		printf -v \
+			${COMMAND} \
+			-- '%s' \
+			${COMMAND_PATH}
+	done
+
+	if [[ -f ./Makefile ]]; then
+		make install start
+	else
+		printf -- \
+			"${COLOUR_NEGATIVE}--->${COLOUR_RESET} %s\n" \
+			'ERROR: Missing Makefile.'
+	fi
+
+}
 
 function systemd_install ()
 {
 
-	declare -a COMMAND_PATHS=(
+	local COMMAND
+	declare -a local COMMAND_PATHS=(
 		'/usr/bin/systemctl'
 		'/usr/bin/journalctl'
 	)
@@ -55,15 +308,25 @@ function systemd_install ()
 		${SERVICE_UNIT_INSTANCE_NAME}
 
 	# Copy systemd unit-files into place.
+	SERVICE_UNIT_INSTALL_TEMPLATE_PATH=${SERVICE_UNIT_INSTALL_TEMPLATE_NAME}
+	if [[ -f /etc/systemd/system/${SERVICE_UNIT_INSTALL_TEMPLATE_NAME} ]]; then
+		SERVICE_UNIT_INSTALL_TEMPLATE_PATH=/etc/systemd/system/${SERVICE_UNIT_INSTALL_TEMPLATE_NAME}
+	fi
+
+	SERVICE_UNIT_REGISTER_INSTALL_TEMPLATE_PATH=${SERVICE_UNIT_REGISTER_INSTALL_TEMPLATE_NAME}
+	if [[ -f /etc/systemd/system/${SERVICE_UNIT_REGISTER_INSTALL_TEMPLATE_NAME} ]]; then
+		SERVICE_UNIT_REGISTER_INSTALL_TEMPLATE_PATH=/etc/systemd/system/${SERVICE_UNIT_REGISTER_INSTALL_TEMPLATE_NAME}
+	fi
+
 	cat \
-		${SERVICE_UNIT_INSTALL_TEMPLATE_NAME} \
+		${SERVICE_UNIT_INSTALL_TEMPLATE_PATH} \
 		> ${CHROOT_DIRECTORY%*/}/etc/systemd/system/${SERVICE_UNIT_TEMPLATE_NAME}
 
 	if [[ ${INSTALL_SERVICE_REGISTER_ENABLED} == true ]]; then
 		sed \
 			-e "s~{{SERVICE_UNIT_NAME}}~${SERVICE_UNIT_NAME}~g" \
 			-e "s~{{SERVICE_UNIT_GROUP}}~${SERVICE_UNIT_GROUP}~g" \
-			${SERVICE_UNIT_REGISTER_INSTALL_TEMPLATE_NAME} \
+			${SERVICE_UNIT_REGISTER_INSTALL_TEMPLATE_PATH} \
 			> ${CHROOT_DIRECTORY%*/}/etc/systemd/system/${SERVICE_UNIT_REGISTER_TEMPLATE_NAME}
 	else
 		# Remove register service unit template if found on host.
@@ -140,7 +403,7 @@ function usage ()
 
 	Options:
 	  -h --help                  Show this help.
-	  -m --manager=MANAGER       Container manager (docker, systemd).
+	  -m --manager=MANAGER       Container manager (docker, make, systemd).
 	  -r --register              Enable the etcd registration service.
 	EOF
 
@@ -187,7 +450,7 @@ while [[ ${#} -gt 0 ]]; do
 			shift 2
 			;;
 		--manager=*)
-			if [[ ${1#*=} != docker ]] && [[ ${1#*=} != systemd ]]; then
+			if [[ ${1#*=} != docker ]] && [[ ${1#*=} != make ]] && [[ ${1#*=} != systemd ]]; then
 				usage
 			fi
 
@@ -213,7 +476,10 @@ case ${INSTALL_SERVICE_MANAGER_TYPE} in
 	systemd)
 		systemd_install
 		;;
+	make)
+		make_install
+		;;
 	docker|*)
-		make install start
+		docker_install
 		;;
 esac
